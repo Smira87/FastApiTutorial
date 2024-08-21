f64803059846dd6cd7bfe24f5643fb04e3c5f54a
Serhii Smirnov <snoff87@gmail.com>
2024-08-21 16:05:50 +0300

    lesson 5 in progress 2

--- auth/database.py
+++ auth/database.py
@@ -2,17 +2,20 @@ from typing import AsyncGenerator
 
 from fastapi import Depends
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
-DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
 class Base(DeclarativeBase):
     pass
 
 
-class User(SQLAlchemyBaseUserTableUUID, Base):
+class User(SQLAlchemyBaseUserTable[int], Base):
     pass
 
 
