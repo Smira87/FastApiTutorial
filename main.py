771c5f54fbdc911db864953aec07930427eb2b1e
Serhii Smirnov <snoff87@gmail.com>
2024-08-24 21:48:12 +0300

    lesson 5 in progress 5

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
+import uuid
+
 from fastapi import FastAPI, Request, status
 from fastapi.exceptions import ResponseValidationError
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from typing import List, Optional
+
+from fastapi_users import fastapi_users, FastAPIUsers
 from pydantic import BaseModel, Field
 from datetime import datetime
 from enum import Enum
 
+from auth import auth_backend
+from auth.database import User
+from auth.manager import get_user_manager
+from auth.schemas import UserRead, UserCreate
+
 app = FastAPI(
     title = "Trading app"
+)
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
 )
