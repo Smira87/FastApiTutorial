0a2444184b2961d127ee264f17773067b51422b4
Serhii Smirnov <snoff87@gmail.com>
2024-08-27 20:48:00 +0300

    lesson 6 ?

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import MetaData, Table, Column, Boolean, TIMESTAMP, Integer, String, ForeignKey, JSON
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 from datetime import datetime
-
+from src.database import Base
 metadata = MetaData()
 
 role = Table(
@@ -25,3 +26,13 @@ user = Table(
     Column("is_verified", Boolean, default=False, nullable=False),
 )
 
+class User(SQLAlchemyBaseUserTable[int], Base):
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    username = Column(String, nullable=False)
+    registered_at = Column(TIMESTAMP, default=datetime.utcnow)
+    role_id = Column(Integer, ForeignKey(role.c.id))
+    hashed_password: str = Column(String(length=1024), nullable=False)
+    is_active: bool = Column(Boolean, default=True, nullable=False)
+    is_superuser: bool = Column(Boolean, default=False, nullable=False)
+    is_verified: bool = Column(Boolean, default=False, nullable=False)
--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,3 +1,5 @@
+from fastapi import Depends
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.database import get_async_session
